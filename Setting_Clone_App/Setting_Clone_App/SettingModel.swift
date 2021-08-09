c19967933c43aadb2505fab370f4b10367d40ce7
tekim <rldod526@naver.com>
2021-08-09 21:54:10 +0900

    Section Study

---
+++ Setting_Clone_App/Setting_Clone_App/SettingModel.swift
@@ -0,0 +1,15 @@
+//
+//  SettingModel.swift
+//  Setting_Clone_App
+//
+//  Created by Kim TaeSoo on 2021/08/09.
+//
+
+import Foundation
+
+struct SettingModel {
+    var leftImageName: String = ""
+    var menuTitle: String = ""
+    var subTitle: String?
+    var rightImageName: String?
+}
--- Setting_Clone_App/Setting_Clone_App/ViewController.swift
+++ Setting_Clone_App/Setting_Clone_App/ViewController.swift
@@ -9,15 +9,32 @@ import UIKit
 // table view 리스트의 경우 하나의 모양(셀)을 만들고 그 모양을 쓰는 방식임
 class ViewController: UIViewController {
 
+    var settingModel = [[SettingModel]]()
+    
+    
     @IBOutlet weak var settingTableView: UITableView!
+    
+    func makeData() {
+        settingModel.append(
+        [SettingModel(leftImageName: "person.circle", menuTitle: "Sign in to your iPhone", subTitle: "Set up iCloud, the APP Store, and more", rightImageName: nil)]
+        )
+        settingModel.append(
+        [SettingModel(leftImageName: "gear", menuTitle: "General", subTitle: nil, rightImageName: "chevron.right"),
+        SettingModel(leftImageName: "person.fill", menuTitle: "Accessibility", subTitle: nil, rightImageName: "chevron.right"),
+        SettingModel(leftImageName: "hand.raised.fill", menuTitle: "Privacy", subTitle: nil, rightImageName: "chevron.right")]
+        )
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         settingTableView.delegate = self
         settingTableView.dataSource = self
+        // section간의 간격의 색을 변경
+        settingTableView.backgroundColor = UIColor(white: 243/255, alpha: 1)
         // cell 을 등록함
         settingTableView.register(UINib(nibName: "ProfileCell", bundle: nil), forCellReuseIdentifier: "ProfileCell")
         settingTableView.register(UINib(nibName: "MenuCell", bundle: nil), forCellReuseIdentifier: "MenuCell")
+        makeData()
     }
 
     
@@ -26,18 +43,35 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (5)
+        return settingModel[section].count
     }
+    
+    // section의 개수
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return settingModel.count
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if (indexPath.row == 0) {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+        if (indexPath.section == 0) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as! ProfileCell
+            cell.topTitle.text = settingModel[indexPath.section][indexPath.row].menuTitle
+            cell.profileImageView.image = UIImage(systemName: settingModel[indexPath.section][indexPath.row].leftImageName)
+            cell.bottomDescription.text = settingModel[indexPath.section][indexPath.row].subTitle
             return (cell)
         }
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath) as! MenuCell
+        // .tintcolr 는 시스템에서 불러온 이미지의 색을 바꿈
+        // indexPath를 검색해보자
+        // section을 구분짓게 하려면 일단 메인 스토리보드가서 table view 누르고 plane -> grouped 로 변경
+        cell.leftImageView.image = UIImage(systemName: settingModel[indexPath.section][indexPath.row].leftImageName)
+        cell.leftImageView.tintColor = .gray
+        cell.middleTitle.text = settingModel[indexPath.section][indexPath.row].menuTitle
+        cell.rightImageView.image = UIImage(systemName: settingModel[indexPath.section][indexPath.row].rightImageName ?? "")
+        cell.rightImageView.tintColor = .gray
         return (cell)
-        
+
     }
     // 모든 cell높이 지정
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
